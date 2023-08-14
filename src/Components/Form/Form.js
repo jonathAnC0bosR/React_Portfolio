95348283c059f2ed58aecdab41850046bff3381d
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-08-14 14:27:15 -0600

    Added a huge state that might be not the best way but the form now works

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 //eslint-disable-next-line
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+//eslint-disable-next-line
+const textRegex = /^[a-zA-Z].*[\s\.]*$/
 
 const Form = () => {
   const [form, setForm] = useState({
@@ -11,8 +13,11 @@ const Form = () => {
     message: "",
   });
 
-  const [inputIsFocus, setInputIsFocus] = useState(false);
-
+  const [emailInputIsFocus, setEmailInputIsFocus] = useState(false);
+  const [messageInput, setMessageInput] = useState(false);
+  const [messageIsValid, setMessageIsValid ] = useState(true);
+  const [nameInputFocus, setNameInputFocus] = useState(false);
+  const [nameIsvalid, setNameIsValid] = useState(true);
   const [emailIsValid, setEmailIsValid] = useState(true);
 
   const handleInputChange = (e) => {
@@ -25,48 +30,77 @@ const Form = () => {
 
   useEffect(() => {
     const identifier = setTimeout(() => {
-      if (inputIsFocus) {
+      if(messageInput) {
+        setMessageIsValid(form.message.trim.length > 0 && form.message.trim().match(textRegex));
+      }
+    }, 1500)
+
+    return () => {
+      clearTimeout(identifier)
+    }
+  }, [form.message, messageInput])
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      if (nameInputFocus) {
+        setNameIsValid(form.name.trim().length >= 0 && form.name.trim().match(textRegex));
+      }
+    }, 1500);
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [form.name, nameInputFocus]);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      if (emailInputIsFocus) {
         setEmailIsValid(
           form.email.trim().length > 1 && form.email.trim().match(emailRegex)
         );
       }
-    }, 1000);
+    }, 1500);
     return () => {
       clearTimeout(identifier);
     };
-  }, [form.email, inputIsFocus]);
+  }, [form.email, emailInputIsFocus]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(form);
   };
 
-  return (
+  return (  
     <form onSubmit={handleSubmit} className="text-white mt-8">
       <div className="flex flex-col">
+        {!nameIsvalid &&  (
+          <p className="text-red-900 text-lg font-semibold">Please enter a valid non-empty name</p>
+        )}
         <label htmlFor="name" className="mb-2 text-slate-400">
           Name
         </label>
         <input
           onChange={handleInputChange}
+          onFocus={() => setNameInputFocus(true)}
           type="text"
           name="name"
           className="mb-12 bg-transparent border-b-slate-400 border-b-2 w-full focus:outline-none "
         />
         {!emailIsValid && (
-          <p className="text-red-900">Please enter a valid email</p>
+          <p className="text-red-900 text-lg font-semibold">Please enter a valid email</p>
         )}
         <label className="mb-2 text-slate-400">Email</label>
         <input
           onChange={handleInputChange}
-          onFocus={() => setInputIsFocus(true)}
+          onFocus={() => setEmailInputIsFocus(true)}
           name="email"
           className="mb-12 bg-transparent border-b-slate-400 border-b-2 w-full focus:outline-none "
         />
+        {!messageIsValid && <p className="text-red-900 text-lg font-semibold">Please enter a valid non-empty message</p>}
         <label className="mb-2 text-slate-400">Message</label>
         <input
           name="message"
           onChange={handleInputChange}
+          onFocus={() => setMessageInput(true)}
           className="bg-transparent border-b-slate-400 border-b-2 w-full focus:outline-none "
         />
       </div>
